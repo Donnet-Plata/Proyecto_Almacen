675dff30c5e36066525b1a1de865a8bd0d34e105
donnet-plata <donnett2846@gmail.com>
2022-02-05 12:29:37 -0800

    commit inicial.

---
+++ index.php
@@ -0,0 +1,397 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <div class="container-fluid">
+    	<div class="row">
+    		<div class="col-md-12">
+    			<ul class="nav nav-pills">
+    				<li class="nav-item">
+    					<a class="nav-link active" href="#">Inicio</a>
+    				</li>
+    				<li class="nav-item">
+    					<a class="nav-link" href="#">Perfil</a>
+    				</li>
+    				<li class="nav-item">
+    					<a class="nav-link disabled" href="#">Notificaciones</a>
+    				</li>
+    				<li class="nav-item dropdown ml-md-auto">
+    					 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Administrador</a>
+    					<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+    						 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+    						<div class="dropdown-divider">
+    						</div> <a class="dropdown-item" href="#">Separated link</a>
+    					</div>
+    				</li>
+    			</ul>
+    			<h3 class="text-center text-primary">
+    				Almacen almacenoso SA de CV.
+    			</h3>
+    			<div class="carousel slide" id="carousel-591509">
+    				<ol class="carousel-indicators">
+    					<li data-slide-to="0" data-target="#carousel-591509" class="active">
+    					</li>
+    					<li data-slide-to="1" data-target="#carousel-591509">
+    					</li>
+    					<li data-slide-to="2" data-target="#carousel-591509">
+    					</li>
+    				</ol>
+    				<div class="carousel-inner">
+    					<div class="carousel-item active">
+    						<img class="d-block w-100" alt="Carousel Bootstrap First" src="https://www.layoutit.com/img/sports-q-c-1600-500-1.jpg" />
+    						<div class="carousel-caption">
+    							<h4>
+    								Compromiso de la empresa
+    							</h4>
+    							<p>
+    								Almacen almacenoso se compromete a brindar la mayor atención posible y cuidar de los productos que generen espacio en su empresa.
+    							</p>
+    						</div>
+    					</div>
+    					<div class="carousel-item">
+    						<img class="d-block w-100" alt="Carousel Bootstrap Second" src="https://www.layoutit.com/img/sports-q-c-1600-500-2.jpg" />
+    						<div class="carousel-caption">
+    							<h4>
+    								Second Thumbnail label
+    							</h4>
+    							<p>
+    								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+    							</p>
+    						</div>
+    					</div>
+    					<div class="carousel-item">
+    						<img class="d-block w-100" alt="Carousel Bootstrap Third" src="https://www.layoutit.com/img/sports-q-c-1600-500-3.jpg" />
+    						<div class="carousel-caption">
+    							<h4>
+    								Third Thumbnail label
+    							</h4>
+    							<p>
+    								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+    							</p>
+    						</div>
+    					</div>
+    				</div> <a class="carousel-control-prev" href="#carousel-591509" data-slide="prev"><span class="carousel-control-prev-icon"></span> <span class="sr-only">Previous</span></a> <a class="carousel-control-next" href="#carousel-591509" data-slide="next"><span class="carousel-control-next-icon"></span> <span class="sr-only">Next</span></a>
+    			</div>
+    			<div class="jumbotron">
+    				<h2>
+    					Hello, world!
+    				</h2>
+    				<p>
+    					This is a template for a simple marketing or informational website. It includes a large callout called the hero unit and three supporting pieces of content. Use it as a starting point to create something more unique.
+    				</p>
+    				<p>
+    					<a class="btn btn-primary btn-large" href="#">Learn more</a>
+    				</p>
+    			</div>
+    			<div class="row">
+    				<div class="col-md-4">
+    					<h3 class="text-center">
+    						h3. Lorem ipsum dolor sit amet.
+    					</h3>
+    					<table class="table table-bordered">
+    						<thead>
+    							<tr>
+    								<th>
+    									#
+    								</th>
+    								<th>
+    									Product
+    								</th>
+    								<th>
+    									Payment Taken
+    								</th>
+    								<th>
+    									Status
+    								</th>
+    							</tr>
+    						</thead>
+    						<tbody>
+    							<tr>
+    								<td>
+    									1
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									01/04/2012
+    								</td>
+    								<td>
+    									Default
+    								</td>
+    							</tr>
+    							<tr class="table-active">
+    								<td>
+    									1
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									01/04/2012
+    								</td>
+    								<td>
+    									Approved
+    								</td>
+    							</tr>
+    							<tr class="table-success">
+    								<td>
+    									2
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									02/04/2012
+    								</td>
+    								<td>
+    									Declined
+    								</td>
+    							</tr>
+    							<tr class="table-warning">
+    								<td>
+    									3
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									03/04/2012
+    								</td>
+    								<td>
+    									Pending
+    								</td>
+    							</tr>
+    							<tr class="table-danger">
+    								<td>
+    									4
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									04/04/2012
+    								</td>
+    								<td>
+    									Call in to confirm
+    								</td>
+    							</tr>
+    						</tbody>
+    					</table>
+    				</div>
+    				<div class="col-md-4">
+    					<h3>
+    						h3. Lorem ipsum dolor sit amet.
+    					</h3>
+    					<table class="table table-bordered">
+    						<thead>
+    							<tr>
+    								<th>
+    									#
+    								</th>
+    								<th>
+    									Product
+    								</th>
+    								<th>
+    									Payment Taken
+    								</th>
+    								<th>
+    									Status
+    								</th>
+    							</tr>
+    						</thead>
+    						<tbody>
+    							<tr>
+    								<td>
+    									1
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									01/04/2012
+    								</td>
+    								<td>
+    									Default
+    								</td>
+    							</tr>
+    							<tr class="table-active">
+    								<td>
+    									1
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									01/04/2012
+    								</td>
+    								<td>
+    									Approved
+    								</td>
+    							</tr>
+    							<tr class="table-success">
+    								<td>
+    									2
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									02/04/2012
+    								</td>
+    								<td>
+    									Declined
+    								</td>
+    							</tr>
+    							<tr class="table-warning">
+    								<td>
+    									3
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									03/04/2012
+    								</td>
+    								<td>
+    									Pending
+    								</td>
+    							</tr>
+    							<tr class="table-danger">
+    								<td>
+    									4
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									04/04/2012
+    								</td>
+    								<td>
+    									Call in to confirm
+    								</td>
+    							</tr>
+    						</tbody>
+    					</table>
+    				</div>
+    				<div class="col-md-4">
+    					<h3 class="text-center">
+    						h3. Lorem ipsum dolor sit amet.
+    					</h3>
+    					<table class="table">
+    						<thead>
+    							<tr>
+    								<th>
+    									#
+    								</th>
+    								<th>
+    									Product
+    								</th>
+    								<th>
+    									Payment Taken
+    								</th>
+    								<th>
+    									Status
+    								</th>
+    							</tr>
+    						</thead>
+    						<tbody>
+    							<tr>
+    								<td>
+    									1
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									01/04/2012
+    								</td>
+    								<td>
+    									Default
+    								</td>
+    							</tr>
+    							<tr class="table-active">
+    								<td>
+    									1
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									01/04/2012
+    								</td>
+    								<td>
+    									Approved
+    								</td>
+    							</tr>
+    							<tr class="table-success">
+    								<td>
+    									2
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									02/04/2012
+    								</td>
+    								<td>
+    									Declined
+    								</td>
+    							</tr>
+    							<tr class="table-warning">
+    								<td>
+    									3
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									03/04/2012
+    								</td>
+    								<td>
+    									Pending
+    								</td>
+    							</tr>
+    							<tr class="table-danger">
+    								<td>
+    									4
+    								</td>
+    								<td>
+    									TB - Monthly
+    								</td>
+    								<td>
+    									04/04/2012
+    								</td>
+    								<td>
+    									Call in to confirm
+    								</td>
+    							</tr>
+    						</tbody>
+    					</table>
+    				</div>
+    			</div>
+    			<address>
+    				 <strong>Almacen almacenoso SA de CV.</strong><br /> Ignacio Comonfort, Zona Urbana Rio Tijuana <br /> Tijuana, BC 22010<br /> <abbr title="Phone"> T: </abbr> (664) 582-6485
+    			</address>
+    		</div>
+    	</div>
+    </div>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
